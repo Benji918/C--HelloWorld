0d8fbf35c9f07e8651a4a6f9b317047422ece30c
Benjamin <kodiugos@gmail.com>
2024-08-11 20:08:06 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a the first number pls!");
+// declare an first int variable
+double myNumber = Convert.ToDouble(Console.ReadLine());
+// print the myNumber variable
+Console.WriteLine($"First number is {myNumber}");
+
+
+Console.WriteLine("The second number pls!");
+// declare the second int variable
+double secondNumber = double.Parse(Console.ReadLine());
+Console.WriteLine($"Second number is {secondNumber}");
+
+//add the two number together
+double total = myNumber + secondNumber;
+Console.WriteLine($"Addition of the numbers {myNumber} and {secondNumber} gives {total}");
+
+
+
+
+Console.ReadKey();
+
